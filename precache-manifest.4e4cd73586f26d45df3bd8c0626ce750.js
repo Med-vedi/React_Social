943afeb69b40e6f10fe57094658d2a3ce9ac4eda
Vladislav Medvedi <desmo_papa@Vladislavs-MacBook-Pro.local>
2020-04-13 20:00:13 +0200

    Updates

---
+++ precache-manifest.4e4cd73586f26d45df3bd8c0626ce750.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c38b88625076772ce2ed",
+    "url": "/React_Social/static/css/main.36d0a764.chunk.css"
+  },
+  {
+    "revision": "c38b88625076772ce2ed",
+    "url": "/React_Social/static/js/main.c38b8862.chunk.js"
+  },
+  {
+    "revision": "4d73f6b06e4aafa1f1a5",
+    "url": "/React_Social/static/js/1.4d73f6b0.chunk.js"
+  },
+  {
+    "revision": "c0d4a45a7f30d4c9ee9e",
+    "url": "/React_Social/static/js/runtime~main.c0d4a45a.js"
+  },
+  {
+    "revision": "dd6ff1ee5c0e2526495cc888e35daaed",
+    "url": "/React_Social/static/media/logo.dd6ff1ee.png"
+  },
+  {
+    "revision": "d2287bb67357f52df58b0b36d7c72f2e",
+    "url": "/React_Social/static/media/ProfileImage.d2287bb6.jpeg"
+  },
+  {
+    "revision": "cf5934aac6deaf5beb30327dae0b6b1e",
+    "url": "/React_Social/index.html"
+  }
+];
